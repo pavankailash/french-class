169630ad33f0acc4d669273ec1b1f8b6fc09176d
Pavan <pavankailash97@live.com>
2026-03-19 11:58:21 +0100

    Add: contents folder and modified config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 //  FRENCH CLASS CONFIG — edit these two lines before deploying
 // =============================================================
 window.FRENCH_CLASS_CONFIG = {
-    REPO_OWNER: 'your-github-username',   // e.g. 'jdupont'
+    REPO_OWNER: 'pavankailash',   // e.g. 'jdupont'
     REPO_NAME:  'french-class',           // your repo name
     BRANCH:     'main'
 };
